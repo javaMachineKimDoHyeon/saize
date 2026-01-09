7c137b05ad6d4028bcba864859ef47ab0990a5fc
dohyeon <ehgus7963@gmail.com>
2026-01-09 10:47:51 +0900

    orderpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,63 @@
 import { useState } from 'react'
 import styled from 'styled-components'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import Start from './pages/Start'
+import NumberOfPeople from './pages/NumberOfPeople'
+
 import './App.css'
 
 function App() {
-
   return (
     <div>
-      <Start></Start>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/numberOfPeople' element={<NumberOfPeople />} />
+        <Route path='/order' element={<Order />} />
+      </Routes>
     </div>
   )
 }
 
-function Start() {
-
+function Order() {
   return (
-    <div className='center'>
-      <div className='box'>
-        <header>いらっしゃいませ</header>
-        <img className='language' src="language.jpg" alt="language" style={{ width: 100, float: 'right' }} />
-        <img src="firstLogo.jpg" alt="logo" />
-        <div style={{ textAlign : 'center'}}>
-          <button className='orderButton'>注文をはじめる</button>
+    <div>
+      <div className='center'>
+        <div className='box'>
+          <header>メニューブックから番号を入力してください</header>
+          <img src="logo.jpg" alt="logo" style={{ width: 200, margin: 'auto'}} />
+          <div className='orderBtnCenter'>
+            <div>
+              <input type="text" className='orderNumber' readOnly/>
+            </div>
+            <div>
+              <button className='orderBtn'>1</button>
+              <button className='orderBtn'>2</button>
+              <button className='orderBtn'>3</button>
+            </div>
+            <div>
+              <button className='orderBtn'>4</button>
+              <button className='orderBtn'>5</button>
+              <button className='orderBtn'>6</button>
+            </div>
+            <div>
+              <button className='orderBtn'>7</button>
+              <button className='orderBtn'>8</button>
+              <button className='orderBtn'>9</button>
+            </div>
+            <div>
+              <button className='orderBtn' style={{ backgroundColor: 'rgba(0,0,0,0)', border: 'none', color: 'rgba(0,0,0,0)'}}>1</button>
+              <button className='orderBtn'>0</button>
+              <button className='orderDelBtn'>削除</button>
+            </div>
+          </div>
+          <footer>
+            <button>注文追加</button>
+            <button>注文かご</button>
+            <button>注文履歴</button>
+            <button>店員呼出</button>
+            <button>会計する</button>
+          </footer>
         </div>
-        <div className='startFooter'>ⓒ2026 Saizeriya Co,. Ltd. All rights reserved.</div>
       </div>
     </div>
   )
