51030ef567edc7702ade0441740d408ae0235084
dohyeon <ehgus7963@gmail.com>
2026-01-14 10:26:21 +0900

    navigate button NumberOfPeople to

--- src/pages/NumberOfPeople.jsx
+++ src/pages/NumberOfPeople.jsx
@@ -1,22 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 function NumberOfPeople() {
+  let navigate = useNavigate()
+  
   return (
     <>
       <header>何名様(全員)でご利用ですか？</header>
       <div className='peopleBtnCenter'>
         <div>
-          <button className='peopleBtn'>1人</button>
-          <button className='peopleBtn'>2人</button>
-          <button className='peopleBtn'>3人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>1人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>2人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>3人</button>
         </div>
         <div>
-          <button className='peopleBtn'>4人</button>
-          <button className='peopleBtn'>5人</button>
-          <button className='peopleBtn'>6人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>4人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>5人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>6人</button>
         </div>
         <div>
-          <button className='peopleBtn'>7人</button>
-          <button className='peopleBtn'>8人</button>
-          <button className='peopleBtn'>9人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>7人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>8人</button>
+          <button className='peopleBtn' onClick={()=> navigate('/order')}>9人</button>
         </div>
       </div>
     </>
