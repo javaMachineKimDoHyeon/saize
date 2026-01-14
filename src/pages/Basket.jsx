4a01ec3848c1f9c2d2ef50f929545d4d33aab0d9
dohyeon <ehgus7963@gmail.com>
2026-01-14 14:40:59 +0900

    TransmitModal

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,31 +1,43 @@
 function Basket() {
-    return(
+    return (
         <div>
-            <header style={{marginBottom : '22px'}}>他に注文があれば「追加」、なければ「注文」</header>
+            <header style={{ marginBottom: '22px' }}>他に注文があれば「追加」、なければ「注文」</header>
             <div className="itemBoxList">
                 <Item />
                 <Item />
                 <Item />
             </div>
-            <div className="basketDetail" style={{marginRight:100}}>2点</div>
-            <div className="basketDetail">合計 600円(税込)</div>
+            <div className="basketDetail" style={{ marginRight: 100 }}>2点</div>
+            <div className="basketDetail">合計 600円 (税込)</div>
             <div>
                 <button className="basketBtn">追　加</button>
-                <button className="basketBtn" style={{background : '#bf261b'}}>注　文</button>
+                <button className="basketBtn" style={{ background: '#bf261b' }}>注　文</button>
             </div>
         </div>
     )
 }
 
 function Item() {
-    return(
+    return (
         <div className="item">
-            <div className="menuName">
+            <div className="menuName" style={{ textAlign: "left", marginLeft: 10 }}>
                 ﾗﾝﾁ)ﾀﾗｺｿｰｽｼｼﾘｰ風
             </div>
-            <button className="countBtn">-</button>
-            <input className="countInput" type="text" />
-            <button className="countBtn">+</button>
+            <div style={{ textAlign: "right" }}>
+                <button className="countBtn">-</button>
+                <input className="countInput" type="text" />
+                <button className="countBtn" style={{ marginRight: 10 }}>+</button>
+            </div>
+        </div>
+    )
+}
+
+function TransmitModal() {
+    return(
+        <div className="transmitModal">
+            <div>ご注文はすべて入力済ですか？送信します。</div>
+            <button>いいえ</button>
+            <button>はい</button>
         </div>
     )
 }
